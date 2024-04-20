8b01958eb0d99416ed77d1489b77131bd2e13f18
thongthai.r@ku.th <thongthai.r@ku.th>
2024-04-21 02:51:22 +0700

    Able to read and print from raw csv data

--- main.go
+++ main.go
@@ -29,6 +29,7 @@ func main() {
 
 	e.POST("/tax/calculations", taxHandler.TaxCalculate)
 	e.POST("/admin/deductions/personal", taxHandler.ChangePersonalDeduction)
+	e.POST("/tax/calculations/upload-csv", taxHandler.ReadTaxCSV)
 
 	go func() {
 		if err := e.Start(":" + os.Getenv("PORT")); err != nil && err != http.ErrServerClosed {
